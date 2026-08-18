e9b8149a7a58b01ce1c5e985c39b350fb77e33da
Matheus Jose Geraldini dos Santos <matheusjgsantos@gmail.com>
2026-08-18 18:01:30 +1000

    Fix SDL2 keyboard mapping via SDL_GetScancodeFromKey and scancode table

--- Src/Pi/PiInput.c
+++ Src/Pi/PiInput.c
@@ -35,7 +35,7 @@
 #include <SDL_keyboard.h>
 
 //static int kbdTable[3][SDLK_LAST];
-static long kbdTable[3][0x00000000];
+static long kbdTable[3][SDL_NUM_SCANCODES];
 
 static int inputTypeScanStart = 0;
 static int inputTypeScanEnd = 1;
@@ -45,138 +45,137 @@ extern Properties *properties;
 static void initKbdTable()
 {
 	memset (kbdTable, 0, sizeof(kbdTable));
+	#define MAP(row, key, ec) do { \
+		SDL_Scancode sc = SDL_GetScancodeFromKey(key); \
+		if(sc != SDL_SCANCODE_UNKNOWN) kbdTable[row][sc] = ec; \
+	} while(0)
 
-	kbdTable[0][SDLK_0] = EC_0;
-	kbdTable[0][SDLK_1] = EC_1;
-	kbdTable[0][SDLK_2] = EC_2;
-	kbdTable[0][SDLK_3] = EC_3;
-	kbdTable[0][SDLK_4] = EC_4;
-	kbdTable[0][SDLK_5] = EC_5;
-	kbdTable[0][SDLK_6] = EC_6;
-	kbdTable[0][SDLK_7] = EC_7;
-	kbdTable[0][SDLK_8] = EC_8;
-	kbdTable[0][SDLK_9] = EC_9;
-
-	kbdTable[0][SDLK_MINUS       ] = EC_NEG;
-	kbdTable[0][SDLK_EQUALS      ] = EC_CIRCFLX;
-	kbdTable[0][SDLK_BACKSLASH   ] = EC_BKSLASH;
-	kbdTable[0][SDLK_LEFTBRACKET ] = EC_AT;
-	kbdTable[0][SDLK_RIGHTBRACKET] = EC_LBRACK;
-	kbdTable[0][SDLK_SEMICOLON   ] = EC_SEMICOL;
-	kbdTable[0][SDLK_QUOTE       ] = EC_COLON;
-	kbdTable[0][SDLK_CARET       ] = EC_RBRACK;
-	kbdTable[0][SDLK_COMMA       ] = EC_COMMA;
-	kbdTable[0][SDLK_PERIOD      ] = EC_PERIOD;
-	kbdTable[0][SDLK_SLASH       ] = EC_DIV;
-	kbdTable[0][SDLK_RCTRL       ] = EC_UNDSCRE;
-
-	kbdTable[0][SDLK_a] = EC_A;
-	kbdTable[0][SDLK_b] = EC_B;
-	kbdTable[0][SDLK_c] = EC_C;
-	kbdTable[0][SDLK_d] = EC_D;
-	kbdTable[0][SDLK_e] = EC_E;
-	kbdTable[0][SDLK_f] = EC_F;
-	kbdTable[0][SDLK_g] = EC_G;
-	kbdTable[0][SDLK_h] = EC_H;
-	kbdTable[0][SDLK_i] = EC_I;
-	kbdTable[0][SDLK_j] = EC_J;
-	kbdTable[0][SDLK_k] = EC_K;
-	kbdTable[0][SDLK_l] = EC_L;
-	kbdTable[0][SDLK_m] = EC_M;
-	kbdTable[0][SDLK_n] = EC_N;
-	kbdTable[0][SDLK_o] = EC_O;
-	kbdTable[0][SDLK_p] = EC_P;
-	kbdTable[0][SDLK_q] = EC_Q;
-	kbdTable[0][SDLK_r] = EC_R;
-	kbdTable[0][SDLK_s] = EC_S;
-	kbdTable[0][SDLK_t] = EC_T;
-	kbdTable[0][SDLK_u] = EC_U;
-	kbdTable[0][SDLK_v] = EC_V;
-	kbdTable[0][SDLK_w] = EC_W;
-	kbdTable[0][SDLK_x] = EC_X;
-	kbdTable[0][SDLK_y] = EC_Y;
-	kbdTable[0][SDLK_z] = EC_Z;
-
-	kbdTable[0][SDLK_F1       ] = EC_F1;
-	kbdTable[0][SDLK_F2       ] = EC_F2;
-	kbdTable[0][SDLK_F3       ] = EC_F3;
-	kbdTable[0][SDLK_F4       ] = EC_F4;
-	kbdTable[0][SDLK_F5       ] = EC_F5;
-	kbdTable[0][SDLK_ESCAPE   ] = EC_ESC;
-	kbdTable[0][SDLK_TAB      ] = EC_TAB;
-	kbdTable[0][SDLK_PAGEUP   ] = EC_STOP;
-	kbdTable[0][SDLK_BACKSPACE] = EC_BKSPACE;
-	kbdTable[0][SDLK_END      ] = EC_SELECT;
-	kbdTable[0][SDLK_RETURN   ] = EC_RETURN;
-	kbdTable[0][SDLK_SPACE    ] = EC_SPACE;
-	kbdTable[0][SDLK_HOME     ] = EC_CLS;
-	kbdTable[0][SDLK_INSERT   ] = EC_INS;
-	kbdTable[0][SDLK_DELETE   ] = EC_DEL;
-	kbdTable[0][SDLK_LEFT     ] = EC_LEFT;
-	kbdTable[0][SDLK_UP       ] = EC_UP;
-	kbdTable[0][SDLK_RIGHT    ] = EC_RIGHT;
-	kbdTable[0][SDLK_DOWN     ] = EC_DOWN;
-
-	kbdTable[0][SDLK_KP_MULTIPLY] = EC_NUMMUL;
-	kbdTable[0][SDLK_KP_PLUS    ] = EC_NUMADD;
-	kbdTable[0][SDLK_KP_DIVIDE  ] = EC_NUMDIV;
-	kbdTable[0][SDLK_KP_MINUS   ] = EC_NUMSUB;
-	kbdTable[0][SDLK_KP_PERIOD  ] = EC_NUMPER;
-	kbdTable[0][SDLK_PAGEDOWN   ] = EC_NUMCOM;
-	kbdTable[0][SDLK_KP_0] = EC_NUM0;
-	kbdTable[0][SDLK_KP_1] = EC_NUM1;
-	kbdTable[0][SDLK_KP_2] = EC_NUM2;
-	kbdTable[0][SDLK_KP_3] = EC_NUM3;
-	kbdTable[0][SDLK_KP_4] = EC_NUM4;
-	kbdTable[0][SDLK_KP_5] = EC_NUM5;
-	kbdTable[0][SDLK_KP_6] = EC_NUM6;
-	kbdTable[0][SDLK_KP_7] = EC_NUM7;
-	kbdTable[0][SDLK_KP_8] = EC_NUM8;
-	kbdTable[0][SDLK_KP_9] = EC_NUM9;
-
-	kbdTable[0][SDLK_LGUI  ] = EC_TORIKE;
-	kbdTable[0][SDLK_RGUI  ] = EC_JIKKOU;
-	kbdTable[0][SDLK_LSHIFT  ] = EC_LSHIFT;
-	kbdTable[0][SDLK_RSHIFT  ] = EC_RSHIFT;
-	kbdTable[0][SDLK_LCTRL   ] = EC_CTRL;
-	kbdTable[0][SDLK_LALT    ] = EC_GRAPH;
-	kbdTable[0][SDLK_RALT    ] = EC_CODE;
-	kbdTable[0][SDLK_CAPSLOCK] = EC_CAPS;
-	kbdTable[0][SDLK_KP_ENTER] = EC_PAUSE;
-	kbdTable[0][SDLK_SYSREQ  ] = EC_PRINT;
-
-	/*kbdTable[1][SDLK_SPACE       ] = EC_JOY1_BUTTON1;
-	kbdTable[1][SDLK_LCTRL       ] = EC_JOY1_BUTTON2;
-	kbdTable[1][SDLK_LEFT        ] = EC_JOY1_LEFT;
-	kbdTable[1][SDLK_UP          ] = EC_JOY1_UP;
-	kbdTable[1][SDLK_RIGHT       ] = EC_JOY1_RIGHT;
-	kbdTable[1][SDLK_DOWN        ] = EC_JOY1_DOWN;
-	kbdTable[1][SDLK_0           ] = EC_COLECO1_0;
-	kbdTable[1][SDLK_1           ] = EC_COLECO1_1;
-	kbdTable[1][SDLK_2           ] = EC_COLECO1_2;
-	kbdTable[1][SDLK_3           ] = EC_COLECO1_3;
-	kbdTable[1][SDLK_4           ] = EC_COLECO1_4;
-	kbdTable[1][SDLK_5           ] = EC_COLECO1_5;
-	kbdTable[1][SDLK_6           ] = EC_COLECO1_6;
-	kbdTable[1][SDLK_7           ] = EC_COLECO1_7;
-	kbdTable[1][SDLK_8           ] = EC_COLECO1_8;
-	kbdTable[1][SDLK_9           ] = EC_COLECO1_9;
-	kbdTable[1][SDLK_MINUS       ] = EC_COLECO1_STAR;
-	kbdTable[1][SDLK_EQUALS      ] = EC_COLECO1_HASH;
-
-	kbdTable[2][SDLK_KP_0     ] = EC_COLECO2_0;
-	kbdTable[2][SDLK_KP_1     ] = EC_COLECO2_1;
-	kbdTable[2][SDLK_KP_2     ] = EC_COLECO2_2;
-	kbdTable[2][SDLK_KP_3     ] = EC_COLECO2_3;
-	kbdTable[2][SDLK_KP_4     ] = EC_COLECO2_4;
-	kbdTable[2][SDLK_KP_5     ] = EC_COLECO2_5;
-	kbdTable[2][SDLK_KP_6     ] = EC_COLECO2_6;
-	kbdTable[2][SDLK_KP_7     ] = EC_COLECO2_7;
-	kbdTable[2][SDLK_KP_8     ] = EC_COLECO2_8;
-	kbdTable[2][SDLK_KP_9     ] = EC_COLECO2_9;
-	kbdTable[2][SDLK_KP_MULTIPLY    ] = EC_COLECO2_STAR;
-	kbdTable[2][SDLK_KP_DIVIDE      ] = EC_COLECO2_HASH;*/
+	MAP(0, SDLK_0, EC_0);
+	MAP(0, SDLK_1, EC_1);
+	MAP(0, SDLK_2, EC_2);
+	MAP(0, SDLK_3, EC_3);
+	MAP(0, SDLK_4, EC_4);
+	MAP(0, SDLK_5, EC_5);
+	MAP(0, SDLK_6, EC_6);
+	MAP(0, SDLK_7, EC_7);
+	MAP(0, SDLK_8, EC_8);
+	MAP(0, SDLK_9, EC_9);
+	MAP(0, SDLK_MINUS, EC_NEG);
+	MAP(0, SDLK_EQUALS, EC_CIRCFLX);
+	MAP(0, SDLK_BACKSLASH, EC_BKSLASH);
+	MAP(0, SDLK_LEFTBRACKET, EC_AT);
+	MAP(0, SDLK_RIGHTBRACKET, EC_LBRACK);
+	MAP(0, SDLK_SEMICOLON, EC_SEMICOL);
+	MAP(0, SDLK_QUOTE, EC_COLON);
+	MAP(0, SDLK_CARET, EC_RBRACK);
+	MAP(0, SDLK_COMMA, EC_COMMA);
+	MAP(0, SDLK_PERIOD, EC_PERIOD);
+	MAP(0, SDLK_SLASH, EC_DIV);
+	MAP(0, SDLK_RCTRL, EC_UNDSCRE);
+	MAP(0, SDLK_a, EC_A);
+	MAP(0, SDLK_b, EC_B);
+	MAP(0, SDLK_c, EC_C);
+	MAP(0, SDLK_d, EC_D);
+	MAP(0, SDLK_e, EC_E);
+	MAP(0, SDLK_f, EC_F);
+	MAP(0, SDLK_g, EC_G);
+	MAP(0, SDLK_h, EC_H);
+	MAP(0, SDLK_i, EC_I);
+	MAP(0, SDLK_j, EC_J);
+	MAP(0, SDLK_k, EC_K);
+	MAP(0, SDLK_l, EC_L);
+	MAP(0, SDLK_m, EC_M);
+	MAP(0, SDLK_n, EC_N);
+	MAP(0, SDLK_o, EC_O);
+	MAP(0, SDLK_p, EC_P);
+	MAP(0, SDLK_q, EC_Q);
+	MAP(0, SDLK_r, EC_R);
+	MAP(0, SDLK_s, EC_S);
+	MAP(0, SDLK_t, EC_T);
+	MAP(0, SDLK_u, EC_U);
+	MAP(0, SDLK_v, EC_V);
+	MAP(0, SDLK_w, EC_W);
+	MAP(0, SDLK_x, EC_X);
+	MAP(0, SDLK_y, EC_Y);
+	MAP(0, SDLK_z, EC_Z);
+	MAP(0, SDLK_F1, EC_F1);
+	MAP(0, SDLK_F2, EC_F2);
+	MAP(0, SDLK_F3, EC_F3);
+	MAP(0, SDLK_F4, EC_F4);
+	MAP(0, SDLK_F5, EC_F5);
+	MAP(0, SDLK_ESCAPE, EC_ESC);
+	MAP(0, SDLK_TAB, EC_TAB);
+	MAP(0, SDLK_PAGEUP, EC_STOP);
+	MAP(0, SDLK_BACKSPACE, EC_BKSPACE);
+	MAP(0, SDLK_END, EC_SELECT);
+	MAP(0, SDLK_RETURN, EC_RETURN);
+	MAP(0, SDLK_SPACE, EC_SPACE);
+	MAP(0, SDLK_HOME, EC_CLS);
+	MAP(0, SDLK_INSERT, EC_INS);
+	MAP(0, SDLK_DELETE, EC_DEL);
+	MAP(0, SDLK_LEFT, EC_LEFT);
+	MAP(0, SDLK_UP, EC_UP);
+	MAP(0, SDLK_RIGHT, EC_RIGHT);
+	MAP(0, SDLK_DOWN, EC_DOWN);
+	MAP(0, SDLK_KP_MULTIPLY, EC_NUMMUL);
+	MAP(0, SDLK_KP_PLUS, EC_NUMADD);
+	MAP(0, SDLK_KP_DIVIDE, EC_NUMDIV);
+	MAP(0, SDLK_KP_MINUS, EC_NUMSUB);
+	MAP(0, SDLK_KP_PERIOD, EC_NUMPER);
+	MAP(0, SDLK_PAGEDOWN, EC_NUMCOM);
+	MAP(0, SDLK_KP_0, EC_NUM0);
+	MAP(0, SDLK_KP_1, EC_NUM1);
+	MAP(0, SDLK_KP_2, EC_NUM2);
+	MAP(0, SDLK_KP_3, EC_NUM3);
+	MAP(0, SDLK_KP_4, EC_NUM4);
+	MAP(0, SDLK_KP_5, EC_NUM5);
+	MAP(0, SDLK_KP_6, EC_NUM6);
+	MAP(0, SDLK_KP_7, EC_NUM7);
+	MAP(0, SDLK_KP_8, EC_NUM8);
+	MAP(0, SDLK_KP_9, EC_NUM9);
+	MAP(0, SDLK_LGUI, EC_TORIKE);
+	MAP(0, SDLK_RGUI, EC_JIKKOU);
+	MAP(0, SDLK_LSHIFT, EC_LSHIFT);
+	MAP(0, SDLK_RSHIFT, EC_RSHIFT);
+	MAP(0, SDLK_LCTRL, EC_CTRL);
+	MAP(0, SDLK_LALT, EC_GRAPH);
+	MAP(0, SDLK_RALT, EC_CODE);
+	MAP(0, SDLK_CAPSLOCK, EC_CAPS);
+	MAP(0, SDLK_KP_ENTER, EC_PAUSE);
+	MAP(0, SDLK_SYSREQ, EC_PRINT);
+	MAP(1, SDLK_SPACE, EC_JOY1_BUTTON1);
+	MAP(1, SDLK_LCTRL, EC_JOY1_BUTTON2);
+	MAP(1, SDLK_LEFT, EC_JOY1_LEFT);
+	MAP(1, SDLK_UP, EC_JOY1_UP);
+	MAP(1, SDLK_RIGHT, EC_JOY1_RIGHT);
+	MAP(1, SDLK_DOWN, EC_JOY1_DOWN);
+	MAP(1, SDLK_0, EC_COLECO1_0);
+	MAP(1, SDLK_1, EC_COLECO1_1);
+	MAP(1, SDLK_2, EC_COLECO1_2);
+	MAP(1, SDLK_3, EC_COLECO1_3);
+	MAP(1, SDLK_4, EC_COLECO1_4);
+	MAP(1, SDLK_5, EC_COLECO1_5);
+	MAP(1, SDLK_6, EC_COLECO1_6);
+	MAP(1, SDLK_7, EC_COLECO1_7);
+	MAP(1, SDLK_8, EC_COLECO1_8);
+	MAP(1, SDLK_9, EC_COLECO1_9);
+	MAP(1, SDLK_MINUS, EC_COLECO1_STAR);
+	MAP(1, SDLK_EQUALS, EC_COLECO1_HASH);
+	MAP(2, SDLK_KP_0, EC_COLECO2_0);
+	MAP(2, SDLK_KP_1, EC_COLECO2_1);
+	MAP(2, SDLK_KP_2, EC_COLECO2_2);
+	MAP(2, SDLK_KP_3, EC_COLECO2_3);
+	MAP(2, SDLK_KP_4, EC_COLECO2_4);
+	MAP(2, SDLK_KP_5, EC_COLECO2_5);
+	MAP(2, SDLK_KP_6, EC_COLECO2_6);
+	MAP(2, SDLK_KP_7, EC_COLECO2_7);
+	MAP(2, SDLK_KP_8, EC_COLECO2_8);
+	MAP(2, SDLK_KP_9, EC_COLECO2_9);
+	MAP(2, SDLK_KP_MULTIPLY, EC_COLECO2_STAR);
+	MAP(2, SDLK_KP_DIVIDE, EC_COLECO2_HASH);
+	#undef MAP
 }
+
 
 void piInputResetJoysticks()
 {
@@ -238,33 +237,32 @@ void keyboardInit(Properties *properties)
 void keyboardUpdate(SDL_KeyboardEvent *event)
 {
 	int i;
+	SDL_Scancode sc;
 	for (i = inputTypeScanStart; i <= inputTypeScanEnd; i++) {
-		//fprintf(stderr,"keysym.scancode=%16x\n",event->keysym.scancode);
-		//fprintf(stderr,"keysym.sym=%16x\n",event->keysym.sym);
+		sc = SDL_GetScancodeFromKey(event->keysym.sym);
+		if (sc == SDL_SCANCODE_UNKNOWN) continue;
 		if (event->keysym.scancode == 0x7A) {
 			if (event->type == SDL_KEYUP) 
-				inputEventUnset(kbdTable[i][SDLK_RALT]);
+				inputEventUnset(kbdTable[i][SDL_GetScancodeFromKey(SDLK_RALT)]);
 			else if (event->type == SDL_KEYDOWN)
-				inputEventSet(kbdTable[i][SDLK_RALT]);
+				inputEventSet(kbdTable[i][SDL_GetScancodeFromKey(SDLK_RALT)]);
 		} else if (event->type == SDL_KEYUP) {
 			if (event->keysym.scancode == 0x2e) {
 				inputEventUnset(EC_CIRCFLX);
 			} else if (event->keysym.scancode == 0x3b) {
 				inputEventUnset(EC_F2);
 			} else {
-				inputEventUnset(kbdTable[i][event->keysym.sym]);
+				long code = kbdTable[i][sc];
+				if (code) inputEventUnset(code);
 			}
 		} else if (event->type == SDL_KEYDOWN) {
 			if (event->keysym.scancode == 0x2e) {
-				//inputEventSet(kbdTable[i][SDLK_EQUALS]);
 				inputEventSet(EC_CIRCFLX);
-				//fprintf(stderr,"BYPASSING: keysym:%16x, kbdTable entry:%16x\n",event->keysym.sym,kbdTable[0][SDLK_EQUALS]);
 			} else if (event->keysym.scancode == 0x3b) {
 				inputEventSet(EC_F2);
-				//fprintf(stderr,"BYPASSING: keysym:%16x, kbdTable entry:%16x\n",event->keysym.sym,kbdTable[0][event->keysym.sym]);
 			} else {
-			inputEventSet(kbdTable[i][event->keysym.sym]);
-			//fprintf(stderr,"keysym:%16x, kbdTable entry:%16x\n",event->keysym.sym,kbdTable[0][event->keysym.sym]);
+				long code = kbdTable[i][sc];
+				if (code) inputEventSet(code);
 			}
 		}
 	}
